361477beaa47b949a68979d913c29d4842110c2d
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-04-01 16:29:31 +0900

    [WIP] CI fails when there is a warning such as Unused Imports

--- sample-app/src/test/scala/example/application/http/controller/DefaultBoxOfficeResourceSpec.scala
+++ sample-app/src/test/scala/example/application/http/controller/DefaultBoxOfficeResourceSpec.scala
@@ -4,6 +4,9 @@ import example.RouteSpecBase
 import example.application.http._
 import example.usecase.{ BoxOfficeReadModelUseCase, BoxOfficeUseCase }
 
+// CI fails when there is a warning such as Unused Imports
+import scala.concurrent._
+
 final class DefaultBoxOfficeResourceSpec extends RouteSpecBase with BoxOfficeResourceBehaviors {
 
   private def newResource(
