19b05795473f0d69973e2550abb996fc8b21bdb7
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-04-06 15:43:54 +0900

    Test MainDiDesign resolves all dependencies

---
+++ sample-app/src/test/scala/example/MainDiDesignSpec.scala
@@ -0,0 +1,12 @@
+package example
+
+import com.typesafe.config.ConfigFactory
+
+final class MainDiDesignSpec extends ActorSpecBase(ConfigFactory.load("test-akka-cluster")) {
+  "MainDiDesign should resolve all dependencies" in {
+    // withProductionMode を使用することで、
+    // Session を作成できたら、シングルトンの依存関係はすべて解決できていることをテストできる
+    val session = MainDiDesign.design(system.classicSystem).withProductionMode.newSession
+    session.close()
+  }
+}
