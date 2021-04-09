d71bb4c1916b4df0341f6e310f5384688d3cbe28
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-04-09 17:38:24 +0900

    Refer ConcertActor's messages using a qualified name

--- sample-app/src/main/scala/example/model/concert/actor/ConcertActorBehaviorFactory.scala
+++ sample-app/src/main/scala/example/model/concert/actor/ConcertActorBehaviorFactory.scala
@@ -3,8 +3,8 @@ package example.model.concert.actor
 import akka.actor.typed.Behavior
 import akka.persistence.typed.PersistenceId
 import example.model.concert.ConcertId
-import example.model.concert.actor.ConcertActor.Command
+import example.model.concert.actor.ConcertActor
 
 trait ConcertActorBehaviorFactory {
-  def apply(id: ConcertId, persistenceId: PersistenceId): Behavior[Command]
+  def apply(id: ConcertId, persistenceId: PersistenceId): Behavior[ConcertActor.Command]
 }
