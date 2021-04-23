d23048de910de2c823d762c0058240dba39881c9
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-04-23 12:14:17 +0900

    Revise Accord validation result example using pattern match
    To illustrate how to handle accord.Result.

--- exercise-accord-basic/src/main/scala/example/ValidationResultExample.scala
+++ exercise-accord-basic/src/main/scala/example/ValidationResultExample.scala
@@ -11,17 +11,14 @@ object ValidationResultExample extends App {
     location.longitude is between(-180.0, 180.0)
   }
 
-  // バリデーションに成功する場合
-  val validLocation                 = Location(30, 120)
-  val result: com.wix.accord.Result = validate(validLocation)
-  assert(result == Success)
+  // バリデーション結果はパターンマッチ等で確認できる
+  val validLocation                        = Location(30, 120)
+  val successResult: com.wix.accord.Result = validate(validLocation)
+  successResult match {
+    case Success =>
+      println("success")
+    case Failure(violations: Set[Violation]) =>
+      println(s"failure: $violations")
+  }
 
-  // バリデーションに失敗する場合
-  val invalidLocation                = Location(120, 30)
-  val failure: com.wix.accord.Result = validate(invalidLocation)
-  assert(failure.isInstanceOf[com.wix.accord.Failure])
-
-  // com.wix.accord.Failure のインスタンスには、
-  // バリデーション違反になったものが含まれている。
-  // violations: Set[com.wix.accord.Violation]
 }
