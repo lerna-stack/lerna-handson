eca3a84e7680537fc0c0b9638d9d70d338c7d666
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-05-13 17:22:41 +0900

    Revise Exercise 7A
    - Provide some default code
    - Searching "Vector.foreach" will be a part of exercise

--- exercise-scala-basic/src/main/scala/answer/Answer7A.scala
+++ exercise-scala-basic/src/main/scala/answer/Answer7A.scala
@@ -1,6 +1,7 @@
 package answer
 
 object Answer7A extends App {
+
   def reciprocal(source: Vector[Int]): Vector[Double] = {
     source.map(n => {
       if (n == 0) {
@@ -11,9 +12,15 @@ object Answer7A extends App {
     })
   }
 
-  val source: Vector[Int]       = (1 to 10).toVector
-  val converted: Vector[Double] = reciprocal(source)
-  for (x <- converted) {
+  // 1 ~ 10 までの要素に対して変換を実施する
+  val source: Vector[Int] =
+    Vector(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val converted: Vector[Double] =
+    reciprocal(source)
+
+  // 結果を表示してみる
+  converted.foreach { x: Double =>
     println(x)
   }
+
 }
--- exercise-scala-basic/src/main/scala/exercise/Exercise7A.scala
+++ exercise-scala-basic/src/main/scala/exercise/Exercise7A.scala
@@ -2,9 +2,14 @@ package exercise
 
 object Exercise7A extends App {
   /*
-    - Vector[Int] を受け取り、各要素Nについて、0以外なら0、0以外なら1.0/Nと変換して、Vector[Double] を返すメソッドを実装しよう
-    - 1 ~ 10 までの要素を持つ Vector[Int] を作り、メソッドを呼び出した結果を変数に格納しよう
-    - 結果の各要素を println で表示してみよう
+
+   - Vector[Int] を受け取り、各要素Nについて、次のように変換するメソッドを実装しよう
+      - 0以外なら0
+      - 0以外なら1.0/N
+
+   - foreach とはなんだろう？
+     https://www.scala-lang.org/api/2.13.5/scala/collection/immutable/Vector.html で調べてみよう。
+
    */
 
   def reciprocal(source: Vector[Int]): Vector[Double] = {
@@ -12,14 +17,15 @@ object Exercise7A extends App {
     ???
   }
 
-  /*
-  　ここで 実際にメソッドを呼び出してみよう
-    HINT
-    - 1 ~ 10 までの要素を持つ Vector[Int] を作るには
-      (1 to 10).toVector
-    - forを使えば Vector の各要素を処理できる。
-      val vector: Vector[Int] = ???
-      for (x <- vector) { ??? }
-   */
+  // 1 ~ 10 までの要素に対して変換を実施する
+  val source: Vector[Int] =
+    Vector(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val converted: Vector[Double] =
+    reciprocal(source)
+
+  // 結果を表示してみる
+  converted.foreach { x: Double =>
+    println(x)
+  }
 
 }
