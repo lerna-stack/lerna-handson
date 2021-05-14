061ab5699143c8937e48dfde5ebfe90c96dd0990
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-05-14 15:52:43 +0900

    Merge BoxOfficeServiceBehaviors into DefaultBoxOfficeServiceSpec
    - Inline the tests defined in BoxOfficeServiceBehaviors into DefaultBoxOfficeServiceSpec
    - Remove BoxOfficeServiceBehaviors

--- sample-app/src/test/scala/example/application/command/DefaultBoxOfficeServiceSpec.scala
+++ sample-app/src/test/scala/example/application/command/DefaultBoxOfficeServiceSpec.scala
@@ -2,6 +2,7 @@ package example.application.command
 
 import akka.actor.typed.ActorSystem
 import example.ActorSpecBase
+import example.adapter.{ ConcertIdGenerator, ConcertTicketId }
 import example.adapter.command.BoxOfficeService
 import example.application.ApplicationDIDesign
 import testkit.AirframeDiSessionSupport
@@ -10,8 +11,10 @@ import wvlet.airframe.Design
 final class DefaultBoxOfficeServiceSpec
     extends ActorSpecBase()
     with ClusterShardingSpecLike
-    with BoxOfficeServiceBehaviors
     with AirframeDiSessionSupport {
+
+  import example.adapter.command.BoxOfficeService._
+
   override protected val design: Design =
     ApplicationDIDesign.design
       .bind[ActorSystem[Nothing]].toInstance(system)
@@ -20,8 +23,45 @@ final class DefaultBoxOfficeServiceSpec
     session.build[DefaultBoxOfficeService]
   }
 
-  classOf[DefaultBoxOfficeService].getSimpleName should {
-    behave like boxOfficeService(newService)
+  val idGenerator = new ConcertIdGenerator()
+
+  "create a concert" in {
+    val service        = newService
+    val id             = idGenerator.nextId()
+    val responseFuture = service.createConcert(id, 100)
+    responseFuture.futureValue shouldBe CreateConcertResponse(100)
+  }
+
+  "get the concert" in {
+    val service              = newService
+    val id                   = idGenerator.nextId()
+    val createResponseFuture = service.createConcert(id, 10)
+    createResponseFuture.futureValue shouldBe a[CreateConcertResponse]
+
+    val getResponseFuture = service.getConcert(id)
+    getResponseFuture.futureValue shouldBe
+    GetConcertResponse((1 to 10).map(ConcertTicketId).toVector, cancelled = false)
+  }
+
+  "buy concert tickets" in {
+    val service              = newService
+    val id                   = idGenerator.nextId()
+    val createResponseFuture = service.createConcert(id, 10)
+    createResponseFuture.futureValue shouldBe a[CreateConcertResponse]
+
+    val buyResponseFuture = service.buyConcertTickets(id, 3)
+    buyResponseFuture.futureValue shouldBe
+    BuyConcertTicketsResponse((1 to 3).map(ConcertTicketId).toVector)
+  }
+
+  "cancel the concert" in {
+    val service              = newService
+    val id                   = idGenerator.nextId()
+    val createResponseFuture = service.createConcert(id, 10)
+    createResponseFuture.futureValue shouldBe a[CreateConcertResponse]
+
+    val cancelResponseFuture = service.cancelConcert(id)
+    cancelResponseFuture.futureValue shouldBe CancelConcertResponse(10)
   }
 
 }
